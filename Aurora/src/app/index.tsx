d4b9f3a671a2fae878ba4b4723375e6ff9ce9083
Davi Murta <davimurta@gmail.com>
2025-06-03 00:08:57 -0300

    Ambiente

---
+++ Aurora/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+const App = () => {
+  return (
+    <Text style={{ fontSize: 20, textAlign: "center", margin: 10 }}>
+      Hello, World!
+    </Text>
+  );
+};
+
+export default App;
